2be8bd6622ab05aa19864744864244ea76c2b7fa
Tahawwaa <teenhuntero0o0@gmail.com>
2026-05-12 19:39:54 +0330

    update code

--- PDO/server.php
+++ PDO/server.php
@@ -1,1 +1,20 @@
-<?php ?>
+<?php
+
+$host = "localhost";
+$dbname = "";
+$username = "";
+$password = "";
+
+
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname ,
+        $username,$password"
+    );
+
+    $pdo->setAttribute(PDO::ATTR_AUTOCOMMIT, PDO::ERRMODE_EXCEPTION);
+
+    echo "OK";
+} catch (PDOException $e) {
+    die("NO" . $e->getMessage());
+}
