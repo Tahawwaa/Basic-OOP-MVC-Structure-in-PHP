1f73a4a955219ff12ceda6bd8cb5a397ec50ae54
Tahawwaa <teenhuntero0o0@gmail.com>
2026-06-01 23:39:59 +0330

    update all code and debug code

--- mvc_project/app/bootstrap.php
+++ mvc_project/app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php 
 
-require_once '/libraries/core.php';
-require_once '/libraries/database.php';
-require_once '/libraries/controller.php';
+require_once __DIR__ . '/libraries/core.php';
+require_once __DIR__ . '/libraries/database.php';
+require_once __DIR__ . '/libraries/controller.php';
 
--- mvc_project/app/libraries/core.php
+++ mvc_project/app/libraries/core.php
@@ -1,18 +1,41 @@
 <?php
 
-  class core{
-    protected $currentcontroller = 'pages';
-    protected $currentmethod = 'index';
+class Core {
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
     protected $params = [];
-    
-    public function __construct(){
+
+    public function __construct() {
         $url = $this->getUrl();
-        print_r($url);
+
+        // Load controller
+        if (isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+            $this->currentController = ucwords($url[0]);
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->currentController . '.php';
+        $this->currentController = new $this->currentController;
+
+        // Load method
+        if (isset($url[1]) && method_exists($this->currentController, $url[1])) {
+            $this->currentMethod = $url[1];
+            unset($url[1]);
+        }
+
+        // Get params
+        $this->params = $url ? array_values($url) : [];
+
+        // Call method with params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
-    public function getUrl(){
-        if(isset($_GET['url'])){
-          return $_GET['url'];
+    public function getUrl() {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            return explode('/', $url);
         }
+        return [];
     }
-  }
+}
